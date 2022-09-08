7dd1b3b043f48edaf378e044f081aaedb43067ac
Enrico Freitas <enricofreitas1@gmail.com>
2022-09-08 09:32:46 -0300

    Feat: create some tests - login and POST & GET users

--- src/entities/transactionCategories.entity.ts
+++ src/entities/transactionCategories.entity.ts
@@ -2,7 +2,7 @@ import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
 export default class TransactionCategories {
-  @PrimaryGeneratedColumn("increment", { type: "bigint" })
+  @PrimaryGeneratedColumn("increment", { type: "integer" })
   id: number;
 
   @Column({ length: 2 })
