5b67f0859c64175206de36f1147754ab2a0d8ec1
andrrms <andremarcia4@gmail.com>
2022-09-08 09:41:49 -0400

    feat: user authentication

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,14 @@ import express from "express";
 import profileRoutes from "./routes/profile.routes";
 import usersRoutes from "./routes/users.routes";
 import { handleErrorMiddleware } from "./middleware/handleError.middleware";
+import sessionRoutes from "./routes/session.routes";
 
 const app = express();
 app.use(express.json());
 
 app.use("/profile", profileRoutes);
 app.use("/users", usersRoutes);
+app.use("/login", sessionRoutes);
 
 app.use(handleErrorMiddleware);
 
